528c03cf4209bbc95245eb94e6fb46ba9dcd4417
Dan Booth <DanBoothDev@gmail.com>
2020-03-21 13:42:54 -0300

    Added pushbutton LED with debouncing

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ A collection of arduino sketches.
 ### LEDs
 Who doesn't like LEDs?
 - [Basic LED](led-basic/led-basic.ino) - hello world for LEDs
-- [Button LED](led-button/led-button.ino) - using a pushbutton to control the LED
+- [Pushbutton LED](led-button/led-button.ino) - using a pushbutton to control the LED
+- [Pushbutton LED with debouncing](led-button-debouncing/led-button-debouncing.ino) - using a pushbutton with debouncing to control the LED
+
---
+++ led-button-debouncing/led-button-debouncing.ino
@@ -0,0 +1,42 @@
+/*
+    LED toggle with Pushbutton including simple debouncing
+
+    Turns an LED on when a button is pressed.
+
+    The circuit:
+    * Input  - Pushbutton connected to pin #7
+    * Output - LED connected to pin #13
+
+    Created AUG 2017
+    By Dan Booth
+*/
+
+const int LED = 13;                 // pin for the LED
+const int BUTTON = 7;               // pin for the pushbutton
+const int TRANSITION_DELAY = 10;    // millisecond delay for the pushbutton bouncing
+
+int buttonVal = 0;      // current button state
+int prevButtonVal = 0;  // previous button state
+int ledState = 0;       // led state - 0=off, 1=on
+
+void setup() {
+    pinMode(LED, OUTPUT);   // set the LED is an output
+    pinMode(BUTTON, INPUT); // set the BUTTON is an input
+}
+
+void loop(){
+    buttonVal = digitalRead(BUTTON);  // get the current value of the button
+    
+    // check for a transition from low to high
+    if ((buttonVal == HIGH) && (prevButtonVal == LOW)){
+        ledState = 1 - state;
+        delay(TRANSITION_DELAY);
+    }
+    prevButtonVal = buttonVal;
+
+    if (ledState == 1) {
+        digitalWrite(LED, HIGH);  // turn LED on
+    } else {
+        digitalWrite(LED, LOW);  // turn LED off
+    }
+}
